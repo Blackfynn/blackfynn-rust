3ff98d92e97edcd6cdba15b20dc7aa73c18e58ab
Stephen Cirner <steve@blackfynn.com>
2018-03-19 15:57:49 -0400

    Change from using user/password to api key and secret. Add preferred org put function. This is needed because the api uses this to determine the logged in org.

---
+++ src/bf/api/request/user.rs
@@ -0,0 +1,28 @@
+// Copyright (c) 2018 Blackfynn, Inc. All Rights Reserved.
+
+/// A type representing a user put request
+#[derive(Clone, Hash, PartialEq, Eq, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub organization: Option<String>,
+    pub email: Option<String>,
+    pub url: Option<String>,
+    pub color: Option<String>,
+    pub last_name: Option<String>,
+    pub first_name: Option<String>,
+    pub credential: Option<String>,
+}
+
+impl Default for User {
+    fn default() -> Self {
+        Self {
+            organization: None,
+            email: None,
+            url: None,
+            color: None,
+            last_name: None,
+            first_name: None,
+            credential: None,
+        }
+    }
+}
